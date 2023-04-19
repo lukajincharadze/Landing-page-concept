6891179d5b6b0438f54878c88f4008be3f343e2e
Luka Jincharadze <lukajinchara@gmail.com>
2023-04-19 21:54:04 +0400

    updated
    fixed burgers functionality, its now adjusted perfectly but its raw, it needs some transitions to make it prettier and easier to interface itself.

--- js/javascript.js
+++ js/javascript.js
@@ -1,16 +1,32 @@
 function toggleElements() {
     const greydiv = document.getElementsByClassName('GreyDiv')[0];
     const navBarLinks = document.getElementById('navbarLinks');
+    const shapeContainer = document.getElementsByClassName('shape-container')[0];
   
-    if (greydiv.style.height === '150%') {
-      greydiv.style.height = '100%';
+    if (greydiv.style.height === '65%') {
+      greydiv.style.height = '55%';
       navBarLinks.classList.remove('active');
+      shapeContainer.style.height = '420px';
+      
     } else {
-      greydiv.style.height = '150%';
+      greydiv.style.height = '65%';
       navBarLinks.classList.add('active');
+      shapeContainer.style.height = '582px';
+
     }
   
     burgerIcon.classList.toggle('active');
+
+
+
+
+    // if (shapeContainer.style.height != '420px') {
+    //   shapeContainer.style.height = '647px';
+    //   // navBarLinks.classList.remove('active');
+    // } else {
+    //   shapeContainer.style.height = '420px';
+    //   // navBarLinks.classList.add('active');
+    // }
   }
   
   const burgerIcon = document.getElementById('burgerIcon');
